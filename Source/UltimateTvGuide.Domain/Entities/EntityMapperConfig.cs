544383576ba2fe556a25462b6517d87e80b0f22e
Wesley Donker <wesdonker91@gmail.com>
2017-03-07 15:18:42 +0100

    Updated code with the latest version of the service

--- Source/UltimateTvGuide.Domain/Entities/EntityMapperConfig.cs
+++ Source/UltimateTvGuide.Domain/Entities/EntityMapperConfig.cs
@@ -25,31 +25,56 @@ namespace UltimateTvGuide.Domain.Entities
             return new MapperConfiguration(c =>
             {
                 c.CreateMap<GuideChannelShow, TvShow>()
-                    .ForMember(d => d.StartTime, m => m.ResolveUsing(s => ParseTimeStringToFutureDateTimeOffset(s.StartTime)))
-                    .ForMember(d => d.EndTime, m => m.ResolveUsing(s => ParseTimeStringToFutureDateTimeOffset(s.EndTime)))
-                    .ForMember(d => d.ImdbRating, m => m.ResolveUsing(s => float.Parse(s.IMDBRating, NumberFormatInfo.InvariantInfo)))
-                    .ForMember(d => d.LogoUri, m => m.ResolveUsing(s => !string.IsNullOrEmpty(s.Logo) ? new Uri(s.Logo, UriKind.Absolute) : null));
-
+                    .ForMember(d => d.StartTime, m => m.ResolveUsing(s => TryParseDateTimeOffset(s.StartTime)))
+                    .ForMember(d => d.EndTime, m => m.ResolveUsing(s => TryParseDateTimeOffset(s.EndTime)))
+                    .ForMember(d => d.ImdbRating, m => m.ResolveUsing(s => TryParseRating(s.IMDBRating)))
+                    .ForMember(d => d.LogoUri, m => m.ResolveUsing(s => TryParseUri(s.Logo)));
+                
                 c.CreateMap<GuideChannel, TvChannel>()
                     .ForMember(d => d.Id, m => m.ResolveUsing(s => int.Parse(s.ChannelId)))
                     .ForMember(d => d.Name, m => m.ResolveUsing(s => s.ChannelName))
-                    .ForMember(d => d.Shows, m => m.ResolveUsing(s => s.Show))
-                    .ForMember(d => d.LogoUri, m => m.ResolveUsing(s => !string.IsNullOrEmpty(s.Logo) ? new Uri(s.Logo, UriKind.Absolute) : null));
+                    .ForMember(d => d.LogoUri, m => m.ResolveUsing(s => TryParseUri(s.Logo)))
+                    .ForMember(d => d.Shows, m => m.ResolveUsing(s => s.Show));
 
                 c.CreateMap<Guide, TvGuide>()
-                    .ForMember(d => d.LoadDate, m => m.ResolveUsing(s => DateTimeOffset.ParseExact(s.LoadDate, "dd-MM-yyyy", CultureInfo.InvariantCulture)))
+                    .ForMember(d => d.LoadDate, m => m.ResolveUsing(s => TryParseDateTimeOffset(s.LoadDate)))
                     .ForMember(d => d.Channels, m => m.ResolveUsing(s => s.Channel));
             });
         }
 
-        private static DateTimeOffset ParseTimeStringToFutureDateTimeOffset(string timeString)
+        #region Helper methods
+
+        private static DateTimeOffset TryParseDateTimeOffset(string input)
         {
-            DateTimeOffset now = DateTimeOffset.Now;
-            TimeSpan timeSpan = TimeSpan.ParseExact(timeString, "g", CultureInfo.InvariantCulture);
-            DateTimeOffset result = new DateTimeOffset(now.Year, now.Month, now.Day, timeSpan.Hours, timeSpan.Minutes, now.Second, now.Offset);
-            if (result < now)
-                result = result.AddDays(1);
+            DateTimeOffset result;
+            DateTimeOffset.TryParse(input, out result);
             return result;
         }
+
+        private static int TryParseRating(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            int slashIndex = input.IndexOf('/');
+
+            if (slashIndex <= 0)
+                return 0;
+
+            string ratingString = input.Substring(0, slashIndex);
+
+            int rating;
+            int.TryParse(ratingString, out rating);
+            return rating;
+        }
+
+        private static Uri TryParseUri(string input)
+        {
+            Uri result;
+            Uri.TryCreate(input, UriKind.Absolute, out result);
+            return result;
+        }
+
+        #endregion
     }
 }
--- Source/UltimateTvGuide.Domain/Entities/TvShow.cs
+++ Source/UltimateTvGuide.Domain/Entities/TvShow.cs
@@ -7,7 +7,7 @@ namespace UltimateTvGuide.Domain.Entities
         public string Name { get; set; }
         public DateTimeOffset StartTime { get; set; }
         public DateTimeOffset EndTime { get; set; }
-        public float ImdbRating { get; set; }
+        public int ImdbRating { get; set; }
         public string Plot { get; set; }
         public Uri LogoUri { get; set; }
     }
